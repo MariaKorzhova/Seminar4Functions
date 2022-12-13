9591933798017de6bd1c485afd58a328e04eec9d
Maria Korzhova <mari.shaidullova@bk.ru>
2022-12-13 23:01:32 +0300

    Task 29

---
+++ Task 29/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int arrLenght = 8;
+int[] arr = Auto(arrLenght);
+System.Console.WriteLine(string.Join(" | ", arr));
+
+
+int[] Auto(int len)
+{
+int[] arr = new int[len];
+Random rand = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rand.Next(0, 100);
+}
+return arr;
+}
+
+
